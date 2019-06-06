5be09fddd020888612db7f7d3500d3552a66eac2
chenchen <18306189208@163.com>
2019-06-06 16:15:45 +0800

    1

---
+++ src/router/components.js
@@ -0,0 +1,3 @@
+export default {
+  layout: () => import('@/pages/layout')
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import components from './components'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'layout',
+      component: components['layout']
     }
   ]
 })
