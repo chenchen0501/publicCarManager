d80295fed4f0ce55a6c08f639c48fae60904cce4
chenchen0501 <18306189208@163.com>
2019-07-01 20:50:34 +0800

    1

--- src/permission.js
+++ src/permission.js
@@ -3,22 +3,4 @@ import {getToken} from '@/utils/auth'
 import {childrenRoutes} from '@/router/routes'
 const whiteList = ['/login'] // 不重定向白名单
 router.beforeEach((to, from, next) => {
-  console.log('token', getToken())
-  // cookie中有token则进入，没有进入登录界面
-  if (getToken()) {
-    // console.log('初始路由', router.options.routes) // 获得初始路由
-    if (from.path === 'login') {
-      next('/')
-    } else {
-      next()
-    }
-  } else {
-    console.log('to', to.path)
-    if (whiteList.includes(to.path)) {
-      next()
-    } else {
-      next('/login')
-      next()
-    }
-  }
 })
